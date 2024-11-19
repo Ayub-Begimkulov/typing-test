a48186fc183a334a9412d22da15806c830f9b2bb
Ayub-Begimkulov <begimkulov963@gmail.com>
2024-11-19 20:00:18 +0500

    created basic typing logic

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import { useLatest } from "./hooks/useLatest";
+import { isNumber } from "./utils";
 
-function App() {
-  const [count, setCount] = useState(0)
+/*
+
+- рассчитывать время
+- собирать статистику (слова в минуту + точность)
+- управление клавиатурой (стар / следующий тест)
+- показывать ошибки / прогресс по ходу написания
+
+*/
+
+// const ignoreKeysForStart = [
+//   "Backspace",
+//   "ShiftRight",
+//   "ShiftLeft",
+//   "AltLeft",
+//   "AltRight",
+//   "Space",
+//   "Tab",
+//   "Escape",
+//   "MetaLeft",
+//   "MetaRight",
+//   "F1",
+//   "F2",
+//   "F3",
+//   "F4",
+//   "F5",
+//   "F6",
+//   "F7",
+//   "F8",
+//   "F9",
+//   "F10",
+//   "F11",
+//   "F12",
+// ];
+
+const testInput =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, eaque.";
+
+const TIME_DURATION = 30_000;
+
+export function App() {
+  const [started, setStarted] = useState(false);
+  const [typedText, setTypedText] = useState("");
+  const [timePassed, setTimePassed] = useState(0);
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const latestStarted = useLatest(started);
+
+  console.log(typedText);
+
+  useEffect(() => {
+    let intervalId: number | null = null;
+
+    const handleGlobalKeydown = (event: KeyboardEvent) => {
+      console.log(event);
+      const key = event.key;
+
+      if (latestStarted.current) {
+        return;
+      }
+
+      if (key.length !== 1) {
+        return;
+      }
+      const textArea = textAreaRef.current;
+
+      if (!textArea) {
+        return;
+      }
+
+      textArea.focus();
+      setStarted(true);
+
+      const startTime = performance.now();
+      intervalId = setInterval(() => {
+        const newTimePassed = performance.now() - startTime;
+        setTimePassed(Math.floor(newTimePassed / 1000));
+        if (newTimePassed > TIME_DURATION) {
+          setStarted(false);
+          if (isNumber(intervalId)) {
+            clearInterval(intervalId);
+          }
+          intervalId = null;
+        }
+      }, 1_000);
+    };
+
+    window.addEventListener("keydown", handleGlobalKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleGlobalKeydown);
+      if (isNumber(intervalId)) {
+        clearInterval(intervalId);
+      }
+    };
+  }, []);
+
+  const handleTypedTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTypedText(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>{started ? "Started" : "Start typing"}</div>
+      <div>Time: {timePassed}</div>
+      <div style={{ fontSize: "25px" }}>{testInput}</div>
+      <div>{typedText}</div>
+      <textarea
+        style={{ padding: "0", border: "0", height: 0, overflow: "hidden" }}
+        ref={textAreaRef}
+        value={typedText}
+        onChange={handleTypedTextChange}
+      />
+    </div>
+  );
 }
-
-export default App
